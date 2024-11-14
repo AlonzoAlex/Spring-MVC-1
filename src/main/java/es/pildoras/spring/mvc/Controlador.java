7138e778386727c51603676a6d508205a36fb229
Alonzo <alonzogamarra@gmail.com>
2024-11-14 15:31:10 -0500

    MVC - using xml and @Controller

---
+++ src/main/java/es/pildoras/spring/mvc/Controlador.java
@@ -0,0 +1,15 @@
+package es.pildoras.spring.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
+public class Controlador {
+
+    @RequestMapping("/")
+    public String redirigir(){
+        return "index";
+    }
+    
+}
---
+++ src/main/java/es/pildoras/spring/mvc/Main.java
@@ -0,0 +1,7 @@
+package es.pildoras.spring.mvc;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
